deb52eb1bae14ff4f965728fa9deb141d024139a
Brandon Goldberg <brandonbng23@gmail.com>
2026-05-30 21:55:31 -0400

    Added print statement to display introductory description

--- interface.py
+++ interface.py
@@ -4,3 +4,8 @@ def header(text):
     print("__________________________________________________________________\n")
 
 header("Welcome to Gabbai!")
+print("Gabbai is a system that allows a synagoge and its members to coordinate \n" \
+"Shabbat and Yontif Torah readings in advance. This text-based interface \nis for " \
+"preliminary testing. Copyright Brandon N. Goldberg 2026. \nAll rights reservered. " \
+"No permission is granted to copy, modify, \nredistribute, or use this code without" \
+"explicit written permission.")
