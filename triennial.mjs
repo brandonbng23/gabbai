0d74bd56a684ad97a1008bf7a005e7d4aa9cbc1a
Brandon Goldberg <brandonbng23@gmail.com>
2026-06-03 15:26:35 -0400

    Added getters and setters

--- triennial.mjs
+++ triennial.mjs
@@ -12,4 +12,52 @@ export class Triennial {
         this.tradMaftir = tradMaftir;
         this.yitro = yitro;
     }
+
+    /* Access triennial setting
+     * @returns boolean repersenting schedule is set to follow the triennial schedule (true) or not (false) */
+    getTriennial() {
+        return this.triennial;
+    }
+
+    /* Access maftir fields (triMaftir and tradMaftir) and returns which is set true, or "None" if both are set false 
+     * @returns string repersenting which maftir is set true: "Triennial" if triMaftir, "Traditional" if tradMaftir, or 
+     * "None" if both are set false */
+    getMaftir() {
+        if (this.triMaftir) {
+            return "Triennial";
+        } else if (this.tradMaftir) {
+            return "Traditional";
+        } else {
+            return "None";
+        }
+    }
+
+    /* Accesses Yitro setting determing if the triennial schedule will apply to Parsha Yitro 
+     * @returns boolean repersenting if the triennial schedule will apply to Parsha Yitro (true) or not (false) */
+    getYitro() {
+        return this.yitro;
+    }
+
+    /* Mutates Triennial setting. If triennial is set true, triennial becomes false. If triennial is set false, 
+     * triennial becomes true */
+    toggleTriennial() {
+        this.triennial = !this.triennial;
+    }
+
+    setMaftir(m) {
+        if (m == "tri") {
+            this.triMaftir = true;
+        } else if (m == "trad") {
+            this.tradMaftir = true;
+        } else if (m == "none") {
+            this.triMaftir = false;
+            this.tradMaftir = false;
+        }
+    }
+
+    /* Mutates Yitro setting. If Yitro is set true, Yitro becomes false. If Yitro is set false, Yitro becomes
+     * true */
+    toggleYitro() {
+        this.yitro = !this.yitro;
+    }
 }
