497b4c44398aff4bb33f35f945537bfbfad28153
Brandon Goldberg <brandonbng23@gmail.com>
2026-06-04 20:50:44 -0400

    In initialization of a Schedule instance, employed a value for @field hhRespect

--- main.mjs
+++ main.mjs
@@ -2,7 +2,7 @@ import { Schedule } from "./schedule.mjs"
 import { Triennial } from "./triennial.mjs"
 
 function main() {
-    let schedule = new Schedule(5786, 3, false, new Triennial(false, false, false, false));
+    let schedule = new Schedule(5786, 3, false, false, new Triennial(false, false, false, false));
     schedule.printSchedule();
 }
 
