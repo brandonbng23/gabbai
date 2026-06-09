e4a9e37b1eef8eb793eb89f9fae59a87e5fe9811
Brandon Goldberg <brandonbng23@gmail.com>
2026-06-09 19:34:44 -0400

    Transferred convertToUser() method to User parent class, added setKey() method, finished implementing revokeAdmin() method

--- admin.mjs
+++ admin.mjs
@@ -5,13 +5,30 @@ export class Admin extends User {
     /* Repersents a user who is a shul's administrator who will be granted permissions regarding 
      * their shul's reading schedule
      *
-     * @field title: string repersenting the title of the position the shul's administrator holds
+     * @field title: string repersenting the title of the position this shul's administrator holds
      * @field shul: instance of Shul for who which the administrator adminstrates
      * @field key: string repersenting the key of the shul for which the administrator administrates
      * - Note that the adminstrator's key is unique to their shul, not to each administrator */
-    constructor(title, shul, key) {
+    constructor(firstN, lastN, email, password, title, shul, key) {
+        super(firstN, lastN, email, password);
         this.title = title;
         this.shul = shul;
         this.key = key;
+    }
+
+    /* Mutates key field
+     * @param key: key for which the key field will mutate to */
+    setKey(key) {
+        this.key = key;
+    }
+
+    /* Revokes an existing Admin's administrator privelages
+     * param r: boolean. If r is set true, administrator privelages will be revoked but admin will retain
+     * user privelages. If r is set false, administraor and user privelages will be revoked. */
+    revokeAdmin(r) {
+        this.shul.removeAdmin();
+        if (r) {
+            this.shul?.(new User(this.firstN, this.lastN, this.email, this.password));
+        }
     }
 }
