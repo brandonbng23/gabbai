de00bc9af319f921ee2778cabe21682dcb6d25f7
Brandon Goldberg <brandonbng23@gmail.com>
2026-06-10 13:55:03 -0400

    Removed extraneous initialization of Triennial instance

--- main.mjs
+++ main.mjs
@@ -1,10 +1,7 @@
 import { Schedule } from "./schedule.mjs"
-import { Triennial } from "./triennial.mjs"
 import { Shul } from "./shul.mjs"
-import { Settings } from "./settings.mjs"
 
 function main() {
-    let triennial = new Triennial(false, false, false, false);
     let shul = new Shul("Test Shul", [], [], null);
 
     let schedule = new Schedule(shul.getSettings());
