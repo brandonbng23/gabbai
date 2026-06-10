346b8b283fc31324707ebd7767714eb6d841a379
Brandon Goldberg <brandonbng23@gmail.com>
2026-06-09 22:41:04 -0400

    Initial commit

---
+++ settings.mjs
@@ -0,0 +1,8 @@
+export class Settings {
+    /* Streamlined class repersenting all administration settings. Fields
+     * are documented in-line. All fields are set to a default value and be
+     * modified with its corresponding methods */
+    constructor() {
+
+    }
+}
