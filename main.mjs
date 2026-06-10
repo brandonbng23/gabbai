731b1c1981a9a6b2eb3954328ffd0bbc9f578855
Brandon Goldberg <brandonbng23@gmail.com>
2026-06-09 20:04:57 -0400

    Created a new Shul object

--- main.mjs
+++ main.mjs
@@ -1,9 +1,12 @@
 import { Schedule } from "./schedule.mjs"
 import { Triennial } from "./triennial.mjs"
+import { Shul } from "./shul.mjs"
 
 function main() {
+    let triennial = new Triennial(false, false, false, false);
     let schedule = new Schedule(5786, 3, false, false, new Triennial(false, false, false, false));
-    schedule.printSchedule();
+    let shul = new Shul("Test Shul", [], [], schedule);
+    shul.getSchedule().printSchedule();
 }
 
 main();
