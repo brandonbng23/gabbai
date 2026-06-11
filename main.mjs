5f182efbe2f9f7819d58b6a62e5f2848c5d83790
Brandon Goldberg <brandonbng23@gmail.com>
2026-06-11 14:59:05 -0400

    Updated some values using methods for continued testing

--- main.mjs
+++ main.mjs
@@ -3,6 +3,8 @@ import { Shul } from "./shul.mjs"
 
 function main() {
     let shul = new Shul("Test Shul", [], [], null);
+    shul.getSettings().setHebYear(5787);
+    shul.getSettings().setTriennial(true);
 
     let schedule = new Schedule(shul.getSettings());
     shul.setSchedule(schedule);
