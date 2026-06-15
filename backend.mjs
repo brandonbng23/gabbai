bfc12509b254a77b4e79cb52447f4f0e02591384
Brandon Goldberg <brandonbng23@gmail.com>
2026-06-15 19:12:54 -0400

    Gets schedule data from backend

--- backend.mjs
+++ backend.mjs
@@ -1,10 +1,14 @@
 import express from "express"
+import { Schedule } from "./schedule.mjs"
+import { Settings } from "./settings.mjs"
+
+let schedule = new Schedule(new Settings(5786));
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Coming soon...')
+app.get("/api/schedule", (req, res) => {
+  res.json(schedule.getScheduleData());
 })
 
 app.listen(port, () => {
