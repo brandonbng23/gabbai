dde7061632cb4dd1a4ff661797252e490bf07a16
Brandon Goldberg <brandonbng23@gmail.com>
2026-06-14 21:24:00 -0400

    Created class to retain only minimal data about a Parsha

---
+++ simpleSchedule.mjs
@@ -0,0 +1,62 @@
+import { HebrewCalendar, 
+         HDate,
+         parshaYear, 
+         getHolidaysOnDate} from '@hebcal/core'
+
+import { Settings } from "./settings.mjs"
+import { LinkedList } from "./linkedList.mjs"
+
+export class simpleSchedule {
+    /* Builds a very simple schedule to accesses only a Parsha name and year
+     *
+     * @field settings: instance of Settings to retain administrator's settings,
+     * namely diaspara (false) or Israeli (true) settings
+     * @field hebYear (optional): number repersenting the Hebrew Year for which to formulate a
+     * schedule upon */
+    constructor(settings, hebYear) {
+        this.settings = settings;
+
+        if (hebYear) {
+            this.hebYear = hebYear;
+        } else {
+            if (settings) {
+                this.hebYear = this.settings.getHebYear();
+            } else {
+                this.hebYear = 1;
+            }
+        }
+    }
+
+    /* Creates raw calendar of parshiyot
+     * @returns array of ParshaEvents */
+    getRawCalendar() {
+        return HebrewCalendar.calendar({
+               year: this.hebYear,
+               isHebrewYear: true,
+               il: this.settings.getIL(),
+               sedrot: true,
+               noHolidays: true,
+               noSpecialShabbat: true,
+               noMinorFast: true,
+               noModern: true,
+               shabbatMevrachim: false
+        })
+    }
+
+    /* Creates a simple schedule retaining only parsha name and year 
+     * @returns: LinkedList instance of Objects holding parsha name and year */
+    createSimpleSchedule() {
+        let rawCal = this.getRawCalendar();
+        let schedule = new LinkedList();
+
+        for (let i = 0; i < rawCal.length; i++) {
+            schedule.append({desc: rawCal[i]
+                                   .getDesc()
+                                   .toLowerCase(),
+                             hebYear: this.hebYear
+                        });
+        }
+
+        return schedule;
+    }
+}
