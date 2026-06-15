38212f3cf9fa17186532f245450b14baa99b30ab
Brandon Goldberg <brandonbng23@gmail.com>
2026-06-14 22:08:56 -0400

    Updated how a hebYear is passed to the new instance of Schedule

--- main.mjs
+++ main.mjs
@@ -3,13 +3,13 @@ import { Schedule } from "./schedule.mjs"
 import { Shul } from "./shul.mjs"
 
 function main() {
-    let settings = new Settings(5787);
+    let settings = new Settings(5788);
     settings.setTriennial(false);
 
     let shul = new Shul("Test Shul", [], [], null, "", "", settings);
     
 
-    let schedule = new Schedule(shul.getSettings(), 5786);
+    let schedule = new Schedule(shul.getSettings(), settings.getHebYear());
     shul.setSchedule(schedule);
     shul.getSettings().setTriennial(true);
     
