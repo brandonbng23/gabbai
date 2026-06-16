2cb75d1141215f3e587151a7b87d9165148a6733
Brandon Goldberg <brandonbng23@gmail.com>
2026-06-15 20:05:07 -0400

    Refers to an instance of shul instead of only an instance of schedule

--- backend.mjs
+++ backend.mjs
@@ -1,14 +1,17 @@
 import express from "express"
 import { Schedule } from "./schedule.mjs"
 import { Settings } from "./settings.mjs"
+import { Shul } from "./shul.mjs"
 
+let shul = new Shul("Test Shul", [], [], null, "testshul.org", "testshul.org/contribute", new Settings(5786));
 let schedule = new Schedule(new Settings(5786));
+shul.setSchedule(schedule);
 
 const app = express()
 const port = 3000
 
-app.get("/api/schedule", (req, res) => {
-  res.json(schedule.getScheduleData());
+app.get("/api/shul", (req, res) => {
+  res.json(shul.getShulData());
 })
 
 app.listen(port, () => {
