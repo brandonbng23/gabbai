ade57959de1a3a1803aabb8a84cc65ab30bb7a69
Brandon Goldberg <brandonbng23@gmail.com>
2026-07-29 19:39:11 -0400

    Added react library, dependencies, and necessary files and infrastructure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/navbar"
+import Aside from "./Components/aside"
+
+function Schedule() {
+  return (
+  <div>
+    <Navbar />
+  </div> );
+}
+
+export default Schedule;
