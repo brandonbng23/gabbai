89468aef48e5ca038718fb828444305c1df89434
Brandon Goldberg <brandonbng23@gmail.com>
2026-07-29 19:39:41 -0400

    Began creating sidebar for schedule dashboard

---
+++ frontend/src/Components/aside.jsx
@@ -0,0 +1,15 @@
+function Aside() {
+    return (
+        <div className="app-layout">
+            <aside className="sidebar">
+                <div className="sidebar-top">
+                    <div className="sidebar-item mx-2">
+                        <a class="icon-link sidebar-link active" href="#"></a>
+                    </div>
+                </div>
+            </aside>
+        </div>
+    )
+}
+
+export default Aside;
