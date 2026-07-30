ce772b7f85119989445731352fe7d0cf4a1c973e
Brandon Goldberg <brandonbng23@gmail.com>
2026-07-29 21:25:16 -0400

    Added and updated styling for sidebar item components

--- frontend/src/Components/sidebar-item.jsx
+++ frontend/src/Components/sidebar-item.jsx
@@ -1,9 +1,9 @@
-function SidebarItem(text, icon, link, active) {
+function SidebarItem({ text, icon, link, active }) {
     return (
         <div className="sidebar-item mx-2">
             <a className={`icon-link sidebar-link ${active ? "active" : ""}`} href={link}>
                 {icon}
-                <span className="sidebar-text">Schedule</span>
+                <span className="sidebar-text">{text}</span>
             </a>
         </div>
     );
