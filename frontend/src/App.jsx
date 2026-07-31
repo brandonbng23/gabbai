7a05d4c7b80f6d6cfe9a2b87206cb88bfb7eb50d
Brandon Goldberg <brandonbng23@gmail.com>
2026-07-31 13:25:03 -0400

    Removed problematic import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import Navbar from "./Components/navbar"
 import Aside from "./Components/aside"
+import Schedule from "./Components/schedule"
 import Footer from "./Components/footer"
+//import ScheduleC from "./Components/schedule";
 
-function Schedule() {
+function App() {
   return (
   <>
     <div className="app-layout">
-      <Navbar />
-      <Aside />
+      <Navbar/>
+      <Aside/>
     </div>
     <Footer />
   </>
     );
 }
 
-export default Schedule;
+export default App;
