fe73255006c62755d78ff01febdf6a2046b4b49f
Brandon Goldberg <brandonbng23@gmail.com>
2026-07-31 15:16:06 -0400

    Created Aliyah class to split function from Readers class

---
+++ core/aliyah.mjs
@@ -0,0 +1,8 @@
+export class Aliyah {
+    constructor(parsha, a, psukim, reader) {
+        this.parsha = parsha;
+        this.a = a;
+        this.psukim = psukim;
+        this.reader = reader;
+    }
+}
