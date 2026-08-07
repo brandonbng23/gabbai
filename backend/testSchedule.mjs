84c5cecf4df85dfe38c60c1eaaf2edd9b29a76c9
Brandon Goldberg <brandonbng23@gmail.com>
2026-08-06 20:59:31 -0400

    Updated testing year to 5787

--- backend/testSchedule.mjs
+++ backend/testSchedule.mjs
@@ -1,6 +1,6 @@
 import { getSchedule } from "./services/scheduleService.mjs";
 import { Settings } from "../core/settings.mjs"; 
 
-const schedule = getSchedule(new Settings());
+const schedule = getSchedule(new Settings(5787));
 
 console.dir(schedule, {depth: null});
