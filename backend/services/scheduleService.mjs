eaacb1ba4149ca8691b8dc8a4119f15648e7fc6f
Brandon Goldberg <brandonbng23@gmail.com>
2026-08-06 21:27:12 -0400

    Implemented an instance of Settings to properly formulate instance of Schedule

--- backend/services/scheduleService.mjs
+++ backend/services/scheduleService.mjs
@@ -1,7 +1,8 @@
-import { Schedule } from "../../core/Schedule.mjs";
+import { Schedule } from "../../core/schedule.mjs";
+import { Settings } from "../../core/settings.mjs"
 
 export function getSchedule(year) {
-    const schedule = new Schedule(year);
+    const schedule = new Schedule(new Settings(year));
 
     return schedule.getScheduleData();
 }
