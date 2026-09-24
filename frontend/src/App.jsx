f1359ef87bcd90e750ca467c8a13bd640c5fed2b
Brandon Goldberg <brandonbng23@gmail.com>
2026-09-24 13:49:40 -0400

    Added and updated components Welcome and Aliyah. Added new div with its own class name.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
 import Navbar from "./Components/navbar"
-import Aside from "./Components/aside"
-import Schedule from "./Components/schedule"
+import Aside from "./Components/aside.tsx"
 import Footer from "./Components/footer"
+import Welcome from "./Components/welcome.tsx"
+import Aliyah from "./Components/aliyah"
 //import ScheduleC from "./Components/schedule";
 
 function App() {
   return (
   <>
     <div className="app-layout">
-      <Navbar/>
-      <Aside/>
+      <Navbar></Navbar>
+      <Aside></Aside>
+      <div className="content-window">
+        <Welcome fName="Brandon"></Welcome>
+        <Aliyah></Aliyah>
+      </div>
+      
     </div>
-    <Footer />
+    <Footer></Footer>
   </>
     );
 }
