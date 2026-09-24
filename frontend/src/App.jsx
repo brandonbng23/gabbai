121843dec534180997345b83569b704bb4f4d2c9
Brandon Goldberg <brandonbng23@gmail.com>
2026-09-24 18:42:10 -0400

    Updated class names. Added Hebrew parshiyot names.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,7 @@ const sampleParshiyot = [
     hDate: "29 Tishrei 5787",
     gregDate: "10 October 2026",
     parsha: "Bereshit",
+    hParsha: "בּראשׁית",
     psukim: ["Genesis 1:1-2:3", "Genesis 2:4-2:19", "Genesis 2:20-3:21", "Genesis 3:22-4:18", "Genesis 4:19-4:22", "Genesis 4:23-5:24", "Genesis 5:25-6:8", "Genesis 6:5-6:8", "Isaiah 42:5-43:10"],
     reader: "",
     user: "",
@@ -26,6 +27,7 @@ const sampleParshiyot = [
     hDate: "6 Cheshvan 5787",
     gregDate: "17 October 2026",
     parsha: "Noach",
+    hParsha: "נח",
     psukim: ["Genesis 6:9-6:22", "Genesis 7:1-7:16", "Genesis 7:17-8:14", "Genesis 8:15-9:17", "Genesis 9:18-10:32", "Genesis 11:1-11:32", "Genesis 11:29-11:32", "Isaiah 54:1-55:5"],
     reader: "",
     user: "",
@@ -40,7 +42,7 @@ function App() {
       <Aside></Aside>
       <div className="content-window">
         <WelcomeBar fName="Brandon"></WelcomeBar>
-        <div className="aliyot">
+        <div className="parshiyot">
           {sampleParshiyot.map((p, index) => <Parsha key={index} {...p}></Parsha>)}
         </div>
       </div>
